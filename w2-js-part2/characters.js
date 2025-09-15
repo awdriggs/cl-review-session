b0075a41d3c4fca173fc86c2e20a7ce3dddaa0ff
Adam Driggers <awdriggs@gmail.com>
2025-09-15 15:55:45 +0200

    update

--- w2-js-part2/characters.js
+++ w2-js-part2/characters.js
@@ -3,25 +3,25 @@ let characters = [
     name: "Mario",
     age: 40, // canon age is never stated; usually portrayed as mid-30s/40
     description: "The heroic plumber of the Mushroom Kingdom, known for his red cap and jumping skills.",
-    imgUrl: "https://static.wikia.nocookie.net/mario/images/d/dc/Mario_%282%29.png/revision/latest?cb=20250720063134"
+    imgUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/5/5c/Mario_by_Shigehisa_Nakaue.png/250px-Mario_by_Shigehisa_Nakaue.png"
   },
   {
     name: "Toad",
     age: 30,
     description: "Toad, also known as (Kinopio キノピオ in Japanese), is a humanoid mushroom that is both a single character and the collective name of the 'Mushroom People' found in the Mushroom Kingdom. ",
-    imgUrl: "https://static.wikia.nocookie.net/mario/images/f/f1/Toad_running_artwork.png/revision/latest?cb=20230825171907"
+    imgUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/b/b9/Toad_by_Shigehisa_Nakaue.png/250px-Toad_by_Shigehisa_Nakaue.png"
   },
   {
     name: "Peach",
     age: 30,
     description: "Often called Princess Peach, she’s kind-hearted and frequently rescued by Mario.",
-    imgUrl: "https://static.wikia.nocookie.net/mario/images/7/7c/Peach_Posing_Alt_3D_Artwork.png/revision/latest?cb=20250809092529"
+    imgUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/1/16/Princess_Peach_Stock_Art.png/190px-Princess_Peach_Stock_Art.png"
   },
   {
     name: "Donkey Kong",
     age: 35,
     description: "Powerful ape and occasional rival-turned-ally of Mario, king of the jungle.",
-    imgUrl: "https://static.wikia.nocookie.net/mario/images/c/c3/Donkey-kong.png/revision/latest?cb=20250521175012"
+    imgUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/d/d4/Donkey_Kong_character.png/250px-Donkey_Kong_character.png"
   },
   {
     name: "Luigi",
@@ -33,12 +33,12 @@ let characters = [
     name: "Bowser",
     age: 45,
     description: "The fire-breathing King of the Koopas, Mario’s main adversary.",
-    imgUrl: "https://static.wikia.nocookie.net/mario/images/3/3f/SMBW_Bowser_Artwork.png/revision/latest?cb=20230910142646"
+    imgUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/d/d1/Bowser_by_Shigehisa_Nakaue.png/250px-Bowser_by_Shigehisa_Nakaue.png"
   },
   {
     name: "Koopa Troopa",
     age: 25,
     description: "A loyal turtle soldier in Bowser’s army, famous for its green shell.",
-    imgUrl: "https://static.wikia.nocookie.net/mario/images/0/0d/Koopa_Troopa_NSMBU_artwork.png/revision/latest?cb=20230228020925"
+    imgUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/5/5f/Koopa_Troopa_by_Shigehisa_Nakaue.png/140px-Koopa_Troopa_by_Shigehisa_Nakaue.png"
   }
 ];
